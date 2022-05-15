94a586fc114e4e8920993526505edd6bbac02af0
Eugene Ignashov <eugene.ign@yandex.ru>
2022-05-16 01:28:44 +0500

    мьmin funkcional

---
+++ IdeaProjects/SpringNoBoobs/src/main/java/web/model/User.java
@@ -0,0 +1,57 @@
+package web.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    @NotEmpty(message = "Not empty")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotEmpty(message = "Not empty")
+    private String lastName;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
